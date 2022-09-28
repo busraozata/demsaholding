0d3c7b7139cfd07a09b88bcec8e5a88599b2d813
busraozata <busraozata1998@gmail.com>
2022-09-28 18:22:44 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
+function inVisible(element) {
+    //Checking if the element is
+    //visible in the viewport
+    var WindowTop = $(window).scrollTop();
+    var WindowBottom = WindowTop + $(window).height();
+    var ElementTop = element.offset().top;
+    var ElementBottom = ElementTop + element.height();
+    //animating the element if it is
+    //visible in the viewport
+    if (ElementBottom <= WindowBottom && ElementTop >= WindowTop)
+      animate(element);
+  }
+  
+  //When the document is ready
+  $(function () {
+    //This is triggered when the
+    //user scrolls the page
+    $(window).scroll(function () {
+      //Checking if each items to animate are
+      //visible in the viewport
+      $("h2[data-max]").each(function () {
+        inVisible($(this));
+      });
+    });
+  });
+  
+  function animate(element) {
+    //Animating the element if not animated before
+    if (!element.hasClass("ms-animated")) {
+      var maxval = element.data("max");
+      var html = element.html();
+      element.addClass("ms-animated");
+      $({
+        countNum: element.html(),
+      }).animate(
+        {
+          countNum: maxval,
+        },
+        {
+          //duration 5 seconds
+          duration: 1000,
+          easing: "linear",
+          step: function () {
+            element.html(Math.floor(this.countNum) + html);
+          },
+          complete: function () {
+            element.html(this.countNum + html);
+          },
+        }
+      );
+    }
+  }
+  
+
+
 const menuToggle = document.querySelector(".menu-toggle");
 const wrapper = document.querySelector(".wrapper");
 const navigation = document.querySelector(".menu-area");
@@ -26,74 +81,23 @@ document.addEventListener("scroll", function () {
   }
 });
 
-function inVisible(element) {
-  //Checking if the element is
-  //visible in the viewport
-  var WindowTop = $(window).scrollTop();
-  var WindowBottom = WindowTop + $(window).height();
-  var ElementTop = element.offset().top;
-  var ElementBottom = ElementTop + element.height();
-  //animating the element if it is
-  //visible in the viewport
-  if (ElementBottom <= WindowBottom && ElementTop >= WindowTop)
-    animate(element);
-}
-
-//When the document is ready
-$(function () {
-  //This is triggered when the
-  //user scrolls the page
-  $(window).scroll(function () {
-    //Checking if each items to animate are
-    //visible in the viewport
-    $("h2[data-max]").each(function () {
-      inVisible($(this));
-    });
-  });
-});
-
-function animate(element) {
-  //Animating the element if not animated before
-  if (!element.hasClass("ms-animated")) {
-    var maxval = element.data("max");
-    var html = element.html();
-    element.addClass("ms-animated");
-    $({
-      countNum: element.html(),
-    }).animate(
-      {
-        countNum: maxval,
-      },
-      {
-        //duration 5 seconds
-        duration: 1000,
-        easing: "linear",
-        step: function () {
-          element.html(Math.floor(this.countNum) + html);
-        },
-        complete: function () {
-          element.html(this.countNum + html);
-        },
-      }
-    );
-  }
-}
 
 const fraction = document.getElementById("fraction");
 const slides = document.querySelectorAll(".slide-item");
 const slideCount = slides.length;
 console.log("iki", slideCount);
-fraction.innerHTML = `<span class="start-count"> 1 /</span> <span class="counter-length">${slideCount}</span>`;
+fraction.innerHTML = `<span class="start-count"> 01 /</span> <span class="counter-length">0${slideCount}</span>`;
 var swiperBanner = new Swiper(".mySwiperBanner", {
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: "#swiper-button-next1",
+    prevEl: "#swiper-button-prev1",
   },
+  effect: "fade",
   on: {
     slideChange: () => {
-      fraction.innerHTML = `<span class="start-count">${
+      fraction.innerHTML = `<span class="start-count">0${
         swiperBanner.realIndex + 1
-      }</span> <span class="counter-length">${slideCount}</span>`;
+      }</span> <span class="counter-length">/0${slideCount}</span>`;
     },
   },
 });
@@ -130,15 +134,14 @@ var mySwiperCurrent = new Swiper(".mySwiperCurrent", {
 });
 
 var swiper = new Swiper(".mySwiperNews", {
-    navigation: {
-        nextEl: "#next2",
-        prevEl: "#prev2",
-      },
+  navigation: {
+    nextEl: "#swiper-button-next3",
+    prevEl: "#swiper-button-prev3",
+  },
   breakpoints: {
     640: {
       slidesPerView: 2,
       spaceBetween: 20,
-      
     },
     768: {
       slidesPerView: 2,
@@ -163,3 +166,47 @@ var swiper = new Swiper(".mySwiperNews", {
     },
   },
 });
+
+function toggleTab(e) {
+  var hrefVal = $(e).attr("href");
+  $(".nav-tabs li").removeClass("active");
+  $('.nav-tabs li[data-active="' + hrefVal + '"]').addClass("active");
+}
+
+/* ------------------------------------------------------------- */
+
+$(".mega-menu .sub-item .dropdown-item").on("mouseover", function () {
+  var imgPath = $(this).attr("data-image");
+
+  $(".mega-menu .img-wrapper img").attr("src", imgPath);
+});
+/* ------------------------------------------------------------- */
+
+var rect = $("#container")[0].getBoundingClientRect();
+  var mouse = { x: 0, y: 0, moved: false };
+
+  $("#container").mousemove(function (e) {
+    mouse.moved = true;
+    mouse.x = e.clientX - rect.left;
+    mouse.y = e.clientY - rect.top;
+  });
+
+  // Ticker event will be called on every frame
+  TweenLite.ticker.addEventListener("tick", function () {
+    if (mouse.moved) {
+      parallaxIt(".slide", 200);
+      parallaxIt("img", -20);
+    }
+    mouse.moved = false;
+  });
+
+  function parallaxIt(target, movement) {
+    TweenMax.to(target, 0.3, {
+      x: ((mouse.x - rect.width / 2) / rect.width) * movement,
+      y: ((mouse.y - rect.height / 2) / rect.height) * movement,
+    });
+  }
+
+  $(window).on("resize scroll", function () {
+    rect = $("#container")[0].getBoundingClientRect();
+  });
